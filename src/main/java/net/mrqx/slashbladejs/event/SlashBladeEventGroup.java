769a9864bad6d01ccd6fd56606ca35b482fd8189
墨染琴弦 <2317249571@qq.com>
2026-05-22 19:14:00 +0800

    v1.1.5

--- src/main/java/net/mrqx/slashbladejs/event/SlashBladeEventGroup.java
+++ src/main/java/net/mrqx/slashbladejs/event/SlashBladeEventGroup.java
@@ -5,65 +5,65 @@ import dev.latvian.mods.kubejs.event.EventHandler;
 
 public class SlashBladeEventGroup {
     public static final EventGroup GROUP = EventGroup.of("SlashBladeJSEvents");
-
+    
     public static final EventHandler BLADE_CHANGE_SA = GROUP.server("bladeChangeSpecialAttack",
-            () -> SlashBladeEventJS.BladeChangeSpecialAttackJSEvent.class);
+        () -> SlashBladeEventJS.BladeChangeSpecialAttackJSEvent.class).hasResult();
     public static final EventHandler BLADE_CHANGE_SE = GROUP.server("bladeChangeSpecialEffect",
-            () -> SlashBladeEventJS.BladeChangeSpecialEffectJSEvent.class);
-
+        () -> SlashBladeEventJS.BladeChangeSpecialEffectJSEvent.class).hasResult();
+    
     public static final EventHandler BLADE_COPY_SA = GROUP.server("copySpecialAttackFromBlade",
-            () -> SlashBladeEventJS.CopySpecialAttackFromBladeJSEvent.class);
+        () -> SlashBladeEventJS.CopySpecialAttackFromBladeJSEvent.class);
     public static final EventHandler BLADE_COPY_SE = GROUP.server("copySpecialEffectFromBlade",
-            () -> SlashBladeEventJS.CopySpecialEffectFromBladeJSEvent.class);
-
+        () -> SlashBladeEventJS.CopySpecialEffectFromBladeJSEvent.class);
+    
     public static final EventHandler PRE_COPY_SA = GROUP.server("preCopySpecialAttackFromBlade",
-            () -> SlashBladeEventJS.PreCopySpecialAttackFromBladeJSEvent.class);
+        () -> SlashBladeEventJS.PreCopySpecialAttackFromBladeJSEvent.class).hasResult();
     public static final EventHandler PRE_COPY_SE = GROUP.server("preCopySpecialEffectFromBlade",
-            () -> SlashBladeEventJS.PreCopySpecialEffectFromBladeJSEvent.class);
-
+        () -> SlashBladeEventJS.PreCopySpecialEffectFromBladeJSEvent.class).hasResult();
+    
     public static final EventHandler PROUD_SOUL_ENCHANT = GROUP.server("proudSoulEnchantment",
-            () -> SlashBladeEventJS.ProudSoulEnchantmentJSEvent.class);
-
+        () -> SlashBladeEventJS.ProudSoulEnchantmentJSEvent.class).hasResult();
+    
     public static final EventHandler REFINE_PROGRESS = GROUP.server("refineProgress",
-            () -> SlashBladeEventJS.RefineProgressJSEvent.class);
+        () -> SlashBladeEventJS.RefineProgressJSEvent.class).hasResult();
     public static final EventHandler REFINE_SETTLEMENT = GROUP.server("refineSettlement",
-            () -> SlashBladeEventJS.RefineSettlementJSEvent.class);
-
+        () -> SlashBladeEventJS.RefineSettlementJSEvent.class).hasResult();
+    
     public static final EventHandler COMBO_STATE_REGISTRY = GROUP.startup("comboStateRegistry",
-            () -> SlashBladeEventJS.ComboStateRegistryEvent.class);
-
+        () -> SlashBladeEventJS.ComboStateRegistryEvent.class);
+    
     public static final EventHandler PLAYER_ANIMATION_REGISTRY = GROUP.startup("playerAnimationRegistryEvent",
-            () -> SlashBladeEventJS.PlayerAnimationRegistryEvent.class);
-
+        () -> SlashBladeEventJS.PlayerAnimationRegistryEvent.class);
+    
     public static final EventHandler POWER_BLADE = GROUP.server("powerBlade",
-            () -> SlashBladeEventJS.PowerBladeEvent.class);
+        () -> SlashBladeEventJS.PowerBladeEvent.class).hasResult();
     public static final EventHandler UPDATE_ATTACK = GROUP.server("updateAttack",
-            () -> SlashBladeEventJS.UpdateAttackEvent.class);
+        () -> SlashBladeEventJS.UpdateAttackEvent.class);
     public static final EventHandler BLADE_STAND_ATTACK = GROUP.server("bladeStandAttack",
-            () -> SlashBladeEventJS.BladeStandAttackEvent.class);
+        () -> SlashBladeEventJS.BladeStandAttackEvent.class).hasResult();
     public static final EventHandler HIT = GROUP.server("hit",
-            () -> SlashBladeEventJS.HitEvent.class);
+        () -> SlashBladeEventJS.HitEvent.class).hasResult();
     public static final EventHandler UPDATE = GROUP.server("update",
-            () -> SlashBladeEventJS.UpdateEvent.class);
+        () -> SlashBladeEventJS.UpdateEvent.class).hasResult();
     public static final EventHandler DO_SLASH = GROUP.server("doSlash",
-            () -> SlashBladeEventJS.DoSlashEvent.class);
-
+        () -> SlashBladeEventJS.DoSlashEvent.class).hasResult();
+    
     public static final EventHandler ADD_KILL_COUNT = GROUP.server("addKillCount",
-            () -> SlashBladeEventJS.AddKillCountEvent.class);
+        () -> SlashBladeEventJS.AddKillCountEvent.class);
     public static final EventHandler ADD_PROUD_SOUL = GROUP.server("addProudSoul",
-            () -> SlashBladeEventJS.AddProudSoulEvent.class);
+        () -> SlashBladeEventJS.AddProudSoulEvent.class);
     public static final EventHandler BLADE_STAND_TICK = GROUP.server("bladeStandTick",
-            () -> SlashBladeEventJS.BladeStandTickEvent.class);
+        () -> SlashBladeEventJS.BladeStandTickEvent.class);
     public static final EventHandler BREAK = GROUP.server("break",
-            () -> SlashBladeEventJS.BreakEvent.class);
+        () -> SlashBladeEventJS.BreakEvent.class).hasResult();
     public static final EventHandler NEXT_COMBO = GROUP.server("nextCombo",
-            () -> SlashBladeEventJS.NextComboEvent.class);
+        () -> SlashBladeEventJS.NextComboEvent.class);
     public static final EventHandler NEXT_OF_TIMEOUT_COMBO = GROUP.server("nextOfTimeOutCombo",
-            () -> SlashBladeEventJS.NextOfTimeOutComboEvent.class);
+        () -> SlashBladeEventJS.NextOfTimeOutComboEvent.class);
     public static final EventHandler CHARGE_ACTION = GROUP.server("chargeAction",
-            () -> SlashBladeEventJS.ChargeActionEvent.class);
+        () -> SlashBladeEventJS.ChargeActionEvent.class).hasResult();
     public static final EventHandler PERFORM_SLASH_ART = GROUP.server("performSlashArt",
-            () -> SlashBladeEventJS.PerformSlashArtEvent.class);
+        () -> SlashBladeEventJS.PerformSlashArtEvent.class).hasResult();
     public static final EventHandler SUMMONED_SWORD_ON_HIT_ENTITY = GROUP.server("summonedSwordOnHitEntity",
-            () -> SlashBladeEventJS.SummonedSwordOnHitEntityEvent.class);
+        () -> SlashBladeEventJS.SummonedSwordOnHitEntityEvent.class);
 }
